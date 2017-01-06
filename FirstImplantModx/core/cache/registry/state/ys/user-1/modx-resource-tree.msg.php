b3b7cde3835571e01816bbf557a4c269862bde69
GalanEvgen <e.galan@i.ua>
2017-01-06 22:53:48 +0200

    'commit'

--- FirstImplantModx/core/cache/registry/state/ys/user-1/modx-resource-tree.msg.php
+++ FirstImplantModx/core/cache/registry/state/ys/user-1/modx-resource-tree.msg.php
@@ -3,4 +3,6 @@ return array (
   0 => '/root',
   1 => '/root/web_0/web_1',
   2 => '/root/web_0',
+  3 => '/root/web_0/web_6',
+  4 => '/root/web_0/web_5',
 );
--- FirstImplantModx/core/cache/registry/state/ys/user-1/modx-tree-element.msg.php
+++ FirstImplantModx/core/cache/registry/state/ys/user-1/modx-tree-element.msg.php
@@ -1,10 +1,10 @@
 <?php
 return array (
   0 => '/root',
-  1 => '/root/n_category/n_category_5',
-  2 => '/root/n_category',
-  3 => '/root/n_type_template',
-  4 => '/root/n_type_chunk',
-  5 => '/root/n_type_tv',
-  6 => '/root/n_type_tv/n_tv_category_5',
+  1 => '/root/n_type_template',
+  2 => '/root/n_type_chunk',
+  3 => '/root/n_type_tv',
+  4 => '/root/n_type_tv/n_tv_category_5',
+  5 => '/root/n_category',
+  6 => '/root/n_category/n_category_1',
 );
